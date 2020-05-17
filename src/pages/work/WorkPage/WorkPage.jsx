edbbb4946a7ef1652776f96278ca91747421d961
gd <wjdrlehs57@gmail.com>
2020-05-18 01:20:00 +0900

    구글맵 연동 테스트 반영

--- src/pages/work/WorkPage/WorkPage.jsx
+++ src/pages/work/WorkPage/WorkPage.jsx
@@ -7,40 +7,87 @@ import { LoginActions } from 'store/actionCreators';
 import { WorkPageContainer } from 'containers/pages';
 import * as firebase from 'firebase';
 import Button from '@material-ui/core/Button';
+import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/api';
 
 class WorkPage extends React.Component {
+  state = {
+    isOpen: false,
+  };
+
+  containerStyle = {
+    width: '400px',
+    height: '400px'
+  };
+
+  center = {
+    lat: 37.772,
+    lng: -122.214
+  };
+
+  position = {
+    lat: 37.772,
+    lng: -122.214
+  }
+
+  position2 = {
+    lat: 37.772,
+    lng: -122.214
+  }
 
   componentDidMount() {
     // this.setTest();
   }
 
-  setTest = () => {
-    const memeRef = this.database.ref('\"test\"/');
-    console.log('key', memeRef.push().key);
-    memeRef.update({
-      // test: 'fuck',
-      test2: 'fuck',
+  onLoad = marker => {
+    this.marker = marker;
+    console.log('marker: ', marker)
+  }
+
+  onClick = marker => {
+    console.log('marker: ', marker);
+    const { isOpen } = this.state;
+    this.setState({
+      isOpen: !isOpen,
     });
   }
 
   render() {
     const { children, theme } = this.props;
+    const { isOpen } = this.state;
+    const mapStyles = {
+      width: '100%',
+      height: '100%',
+   };
     return (
-      <ThemeProvider theme={themes[theme]}>
-        <Button
-          variant="contained"
-          color="primary"
+      <LoadScript
+        googleMapsApiKey="AIzaSyDwKM1Pfr80o1hSX1C0tnHxplUzuaLe_Gw"
+      >
+        <GoogleMap
+          mapContainerStyle={this.containerStyle}
+          center={this.center}
+          zoom={10}
         >
-          {'Hello World'}
-        </Button>
-      </ThemeProvider>
+          { /* Child components, such as markers, info windows, etc. */ }
+          <Marker
+            onLoad={this.onLoad}
+            position={this.position}
+            label='qweqwq'
+            onClick={this.onClick}
+          >
+            {isOpen && <InfoWindow anchor={this.marker}>
+              <div>
+                qweqwq
+                {/* <Button>qweqwe</Button> */}
+              </div>
+            </InfoWindow>}
+          </Marker>
+        </GoogleMap>
+      </LoadScript>
     );
   }
 }
 
 WorkPage.propTypes = {
-  children: PropTypes.node.isRequired,
-  theme: PropTypes.string.isRequired,
   // umpScriptOnload: PropTypes.func.isRequired,
 };
 
