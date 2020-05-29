a525ebf0c768d6f996c2fa3b14280ef35c6dfa7f
gunwoo.cho <whitesky0109@naver.com>
2020-05-29 13:13:31 +0000

    [master][ItemSerachForm] add hiding logic

--- src/pages/ItemSearchForm/ItemSearchFormStyled.jsx
+++ src/pages/ItemSearchForm/ItemSearchFormStyled.jsx
@@ -5,7 +5,7 @@ export const PaperStyled = styled(Paper)`
   width: 450px;
   box-sizing: content-box;
   padding: 5px 10px;
-  display: ${({ isOpen }) => (isOpen ? 'inline-block' : 'inline-block')}
+  display: ${({ isOpen }) => (isOpen ? 'inline-block' : 'none')}
 `;
 
 export const LayerStyled = styled(Box)`
